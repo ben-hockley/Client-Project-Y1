fa59b6a74a2319aced4375bc05447f0d00d6eb6a
alex stephens <stephensa10@cardiff.ac.uk>
2023-11-21 14:30:37 +0000

    Created main block in main page and extended to the create account page

--- Server.py
+++ Server.py
@@ -4,6 +4,12 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/createAccount", methods=['GET'])
+def returnCreateAccount():
+    if request.method == 'GET':
+        return render_template('create Account.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
