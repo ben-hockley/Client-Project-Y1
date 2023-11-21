60105b640732dcc80eebd716e402593c09dcd0c4
Benjamin Hockley <hockleyb@cardiff.ac.uk>
2023-11-21 15:20:55 +0000

    added js file and placeholder for actual functional history

---
+++ userDetails.js
@@ -0,0 +1,5 @@
+// function to overwrite qu
+function printQuizName() {
+    document.getElementsByClassName("quizName")[0].innerHTML = "Quiz Name: Fun Quiz"
+    document.getElementsByClassName("quizScore")[0].innerHTML = "Quiz Score: 9/10"
+}
