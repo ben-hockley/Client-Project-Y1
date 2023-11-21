5800c1f2f4bd13a50cf62530770172ec18e77858
Benjamin Hockley <hockleyb@cardiff.ac.uk>
2023-11-21 15:34:13 +0000

    updates to css

--- userDetails.js
+++ userDetails.js
@@ -1,4 +1,4 @@
-// function to overwrite qu
+// function to write new code in class boxes "quizName" and "quizScore"
 function printQuizName() {
     document.getElementsByClassName("quizName")[0].innerHTML = "Quiz Name: Fun Quiz"
     document.getElementsByClassName("quizScore")[0].innerHTML = "Quiz Score: 9/10"
