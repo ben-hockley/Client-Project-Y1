430c83edad58e5dfca3615f6f4bafe91c4006d98
alex stephens <stephensa10@cardiff.ac.uk>
2023-11-22 10:03:08 +0000

    Fixed errors, inserts new account into database

--- Server.py
+++ Server.py
@@ -16,9 +16,9 @@ def returnHome():
     if request.method == 'GET':
         return render_template('Main Page.html')
 
-@app.route("/submitNewAccount", methods=['GET'])
+@app.route("/submitNewAccount", methods=['POST'])
 def submitNewAccount():
-    if request.method == 'GET':
+    if request.method == 'POST':
         firstName = request.form.get("firstName")
         lastName = request.form.get("lastName")
         userName = request.form.get("username")
@@ -26,11 +26,33 @@ def submitNewAccount():
         try:
             conn = sqlite3.connect('Quiz.db')
             cur = conn.cursor()
-            cur.execute("INSERT INTO User ('Username', 'FirstName','SurName','Password','Admin') VALUES (?,?,?,?,?)", (userName,firstName,lastName,password,"N"))
+            cur.execute(\
+            "INSERT INTO User ('Username', 'FirstName','SurName','Password','Admin') \
+            VALUES (?,?,?,?,?)", (userName,firstName,lastName,password,"N"))
+            print("Successfully inserted " + firstName)
         except Exception as e:
             conn.rollback()
             print("Error during insert")
-        return render_template('Main Page.html')
+            print(e)
+        finally:
+            conn.commit()
+            conn.close()
+            return render_template('Main Page.html')
+
+# @app.route("/usernameExist", methods = ['POST'])
+# def usernameExist():
+#     if request.method == 'POST':
+#         firstName = request.form.get("firstName")
+#         lastName = request.form.get("lastName")
+#         userName = request.form.get("username")
+#         password = request.form.get("password")
+#         conn = sqlite3.connect('Quiz.db')
+#         cur = conn.cursor()
+#         isExist = cur.execute(\
+        
+#         )
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
