39e04bbf781db5ea8a0518a3f1c9fea694c5a585
Frederick <mckayf@cardiff.ac.uk>
2023-11-22 12:22:02 +0000

    Basic look for log on page complete

--- Server.py
+++ Server.py
@@ -4,6 +4,10 @@ import json
 
 app = Flask(__name__)
 
-
+@app.route("/Log on", methods=['GET'])
+def returnSecond():
+    if request.method == 'GET':
+        return render_template('Log on.html')
+        
 if __name__ == "__main__":
     app.run(debug=True)
