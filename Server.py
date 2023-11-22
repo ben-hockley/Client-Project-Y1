f6f9e0ea4a29f374c9d3e3bd1278e9239cfe60a9
alex stephens <stephensa10@cardiff.ac.uk>
2023-11-22 12:05:42 +0000

    Completed SQL functions and checked that username isn't in database

--- Server.py
+++ Server.py
@@ -5,52 +5,62 @@ import sqlite3
 
 app = Flask(__name__)
 
+user = None
 
 @app.route("/createAccount", methods=['GET'])
 def returnCreateAccount():
     if request.method == 'GET':
-        return render_template('Create Account.html')
+        return render_template('Create_Account.html')
 
 @app.route("/mainPage", methods=['GET'])
 def returnHome():
     if request.method == 'GET':
-        return render_template('Main Page.html')
+        return render_template('Main_Page.html')
 
-@app.route("/submitNewAccount", methods=['POST'])
-def submitNewAccount():
+def submitNewAccount(firstName,lastName,userName,password):
+    try:
+        conn = sqlite3.connect('Quiz.db')
+        cur = conn.cursor()
+        cur.execute(\
+        "INSERT INTO User ('Username', 'FirstName','SurName','Password','Admin') \
+        VALUES (?,?,?,?,?)", (userName,firstName,lastName,password,"N"))
+        message = True
+        conn.commit()
+    except Exception as e:
+        message = False
+        conn.rollback()
+    finally:
+        conn.close()
+        print(message)
+        return message
+
+@app.route("/usernameExist", methods = ['POST'])
+def usernameExist():
     if request.method == 'POST':
-        firstName = request.form.get("firstName")
-        lastName = request.form.get("lastName")
+        firstName = request.form.get("firstName").title()
+        lastName = request.form.get("lastName").title()
         userName = request.form.get("username")
         password = request.form.get("password")
         try:
             conn = sqlite3.connect('Quiz.db')
             cur = conn.cursor()
             cur.execute(\
-            "INSERT INTO User ('Username', 'FirstName','SurName','Password','Admin') \
-            VALUES (?,?,?,?,?)", (userName,firstName,lastName,password,"N"))
-            print("Successfully inserted " + firstName)
+            "SELECT Username FROM User WHERE Username = ?",([userName]))
+            isExist = cur.fetchall()
+            cur.close()
+            if isExist == []:
+                if submitNewAccount(firstName,lastName,userName,password) == True:
+                    message = "Welcome to your account, " + firstName
+                    global user
+                    user = userName
+                    return render_template('Account_Details.html', data = message)
+                else:
+                    message = "Error inserting " + firstName
+            else:
+                message = "Username '" + userName + "' already exists."
         except Exception as e:
-            conn.rollback()
-            print("Error during insert")
-            print(e)
-        finally:
-            conn.commit()
-            conn.close()
-            return render_template('Main Page.html')
-
-# @app.route("/usernameExist", methods = ['POST'])
-# def usernameExist():
-#     if request.method == 'POST':
-#         firstName = request.form.get("firstName")
-#         lastName = request.form.get("lastName")
-#         userName = request.form.get("username")
-#         password = request.form.get("password")
-#         conn = sqlite3.connect('Quiz.db')
-#         cur = conn.cursor()
-#         isExist = cur.execute(\
-        
-#         )
+            message = "Error during check"
+        return render_template('Create_Account.html', data = message)
 
 
 
