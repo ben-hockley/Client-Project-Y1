c80078377d73444ef74f36adb52e6bad231c46e8
alex stephens <stephensa10@cardiff.ac.uk>
2023-11-22 13:15:01 +0000

    Created logon function route, checks if user exists and whether password is correct

--- Server.py
+++ Server.py
@@ -1,13 +1,44 @@
 import os
 from flask import Flask, redirect, request,render_template
 import json
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/Log on", methods=['GET'])
-def returnSecond():
+user = None
+
+@app.route("/login", methods=['GET'])
+def returnLogin():
     if request.method == 'GET':
         return render_template('Log on.html')
+
+@app.route("/logonFunction", methods=['POST'])
+def logonFunction():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        try:
+            conn = sqlite3.connect("quizDatabase.db")
+            cur = conn.cursor()
+            cur.execute(\
+            "SELECT Password FROM User WHERE Username = ?",([username]))
+            isExist = cur.fetchall()
+            cur.close()
+            if isExist != []:
+                if isExist[0][0] == password:
+                    global user
+                    user = username
+                    return render_template("Main Page.html")
+                else:
+                    message = "Username and password don't match"
+            else:
+                message = "User not found"
+        except Exception as e:
+            cur.close()
+            print(e)
+            message = "Database error"
+        print(message)
+        return redirect("/login")
         
 if __name__ == "__main__":
     app.run(debug=True)
