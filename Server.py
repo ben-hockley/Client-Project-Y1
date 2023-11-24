cef38202d2291a90ff75d88fccadaa51a1d0c15e
sam carter <carters11@cardiff.ac.uk>
2023-11-24 15:35:04 +0000

    just added some details to the user area of the quiz

--- Server.py
+++ Server.py
@@ -7,52 +7,71 @@ app = Flask(__name__)
 
 user = None
 
+def getQuestion(QuizID):
+    data=[]
+    try:
+        conn = sqlite3.connect('quizDatabase.db')
+        cur = conn.cursor()
+        cur.execute('SELECT Answer, question, QuizName, IsTrue, Questions.QuestionID FROM Answers, Questions, Quiz WHERE Answers.QuestionID = Questions.QuestionID AND Questions.QuizID = Quiz.QuizID AND Quiz.QuizID='+QuizID)
+        data = cur.fetchall()
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+    questions=[]
+    for i in data:
+        questionID=""
+        questionName=""
+        trueAnswer=[]
+        falseAnswer=[]
+        quizName=""
+        found = False
+        for j in questions:
+            if j[3] == i[4]:
+                questionID=i[4]
+                found = True
+                break
+        if found:
+            for j in questions:
+                if j[3]==questionID:
+                    if i[3]=="T":
+                        j[1].append(i[0])
+                    else:
+                        j[2].append(i[0])
+        else:
+            questionName=i[1]
+            if i[3] == "T":
+                trueAnswer.append(i[0])
+            else:
+                falseAnswer.append(i[0])
+            question=(questionName, trueAnswer, falseAnswer, i[4], i[2])
+            questions.append(question)
+    return questions
+
+
+
 @app.route("/userEnd", methods=['GET', 'POST'])
 def userEnd():
+    QuizID="50"
+    UserID="21"
     if request.method == 'GET':
-        return render_template('User End.html')
+        return render_template('User End.html', data=getQuestion(QuizID))
     if request.method == 'POST':
-        data=[]
+        Points=4
         msg=""
         try:
+            keys = request.form.keys()
+            Points = []
+            for i in keys:
+                Points.append(i)
+            print(Points)
             conn = sqlite3.connect('quizDatabase.db')
             cur = conn.cursor()
-            cur.execute('SELECT Answer, question, QuizName, IsTrue, Questions.QuestionID FROM Answers, Questions, Quiz WHERE Answers.QuestionID = Questions.QuestionID AND Questions.QuizID = Quiz.QuizID AND Quiz.QuizID=50')
+            cur.execute('INSERT INTO Players(QuizID, UserID, Points) VALUES (?,?,?)', (QuizID, UserID, Points))
             data = cur.fetchall()
             conn.commit()
-            msg="sent"
         except Exception as e:
             conn.rollback()
-            msg="failed to send"
-        questions=[]
-        for i in data:
-            questionID=""
-            questionName=""
-            trueAnswer=[]
-            falseAnswer=[]
-            quizName=""
-            found = False
-            for j in questions:
-                if j[3] == i[4]:
-                    questionID=i[4]
-                    found = True
-                    break
-            if found:
-                for j in questions:
-                    if j[3]==questionID:
-                        if i[3]=="T":
-                            j[1].append(i[0])
-                        else:
-                            j[2].append(i[0])
-            else:
-                questionName=i[1]
-                if i[3] == "T":
-                    trueAnswer.append(i[0])
-                else:
-                    falseAnswer.append(i[0])
-                question=(questionName, trueAnswer, falseAnswer, i[4], data[3])
-                questions.append(question)
-        return render_template('User End.html', data=questions)    
+        return render_template('Main Page.html')
 
 @app.route("/createAccount", methods=['GET'])
 def returnCreateAccount():
---
+++ static/JS/User End.js
@@ -0,0 +1,80 @@
+const DATA = document.getElementById("DATA");
+var data = DATA.textContent;
+data = data.replace(/'/g, '"');
+data = data.replace(/\(/g, '[').replace(/\)/g, ']');
+data = JSON.parse(data);
+var questionNumber = 0;
+var points = 0;
+var answers = [];
+Setup()
+function Setup(){
+    if(questionNumber<data.length){
+        const answerSection = document.getElementById("AnswerSection");
+        var num = 0;
+        var questions = [];
+        for (let i = 0; i < (data[questionNumber])[1].length; i++){
+            questions.push((data[questionNumber])[1][i]);
+        }
+        
+        for (let i = 0; i < (data[questionNumber])[2].length; i++){
+            questions.push((data[questionNumber])[2][i]);
+        }
+        var numbers=[];
+        for (let i = 0; i < questions.length; i++) {
+            const element = questions[i];
+            numbers.push(i);
+        }
+        for (let i = 0; i < questions.length; i++) {
+            const X = numbers[Math.floor(Math.random() * numbers.length)]
+            numbers = numbers.filter(function(element) {return element !== X;});
+            const element = questions[X];
+            num++;
+            var answerButton = document.createElement("button");
+            answerButton.type="button";
+            answerButton.id="answer"+num;
+            answerButton.textContent=(element);
+            answerButton.setAttribute("onclick", "isItCorrect(event)");
+            answerSection.appendChild(answerButton);
+        }
+        const questionNumberElement = document.getElementById("QuestionNumber");
+        questionNumberElement.textContent="Q"+(questionNumber+1);
+        const question = document.getElementById("Question");
+        question.textContent= (data[questionNumber])[0];
+    }
+    else{
+        const answerSection = document.getElementById("AnswerSection");
+        const questionNumberElement = document.getElementById("QuestionNumber");
+        questionNumberElement.textContent="Quiz";
+        const question = document.getElementById("Question");
+        question.textContent= "You scored "+points+" out of "+data.length+" points";
+        const Submit = document.createElement("button");
+        Submit.setAttribute("onclick", "Submit()")
+        Submit.textContent="Submit score";
+        const point = document.getElementById("POINTS")
+        point.textContent=points;
+        answerSection.appendChild(Submit);
+    }
+}
+function isItCorrect(event) {
+    const answerSection = document.getElementById("AnswerSection");
+    while (answerSection.childNodes.length>0) {
+        const element = answerSection.childNodes[0];    
+        answerSection.removeChild(element);
+    };
+    answers.push(event.target.textContent);
+    correct = data[questionNumber][1]
+    var rewarded=false
+    correct.forEach(answer => {
+        if(answers[answers.length-1]==answer){
+            if(!rewarded){
+                points++; 
+                rewarded=true           
+            }
+        }
+    });
+    questionNumber++;
+    Setup();
+}
+function Submit(){
+
+}
