9518b61707e8a74d7383821aff8ccabb5db33c17
alex stephens <stephensa10@cardiff.ac.uk>
2023-11-27 14:36:23 +0000

    Fixed merge error and connected the create account page to the login page

--- Server.py
+++ Server.py
@@ -6,6 +6,8 @@ import hashlib
 
 app = Flask(__name__)
 
+ALLOWED_EXTENTIONS = set(['jpg', 'txt', 'svg', 'png', 'jpeg', 'gif'])
+
 user = None
 DATABASE = "quizDatabase.db"
 
@@ -104,17 +106,13 @@ def returnCreateAccount():
     if request.method == 'GET':
         return render_template('Create_Account.html')
 
-@app.route("/mainPage", methods=['GET'])
-def returnHome():
-    if request.method == 'GET':
-        return render_template('Main_Page.html')
-
 @app.route("/accountDetails", methods=['GET'])
 def returnAccountDetails():
     global user
     print(user)
     if request.method == 'GET':
         return render_template('Account_Details.html')
+
 
 @app.route("/updateInfo", methods=['GET'])
 def updateInfo():
@@ -306,12 +304,16 @@ def updateLastname():
 
 
 
-@app.route("/login", methods=['GET'])
-def returnLogin():
-    if request.method == 'GET':
-        return render_template('Log on.html')
+@app.route("/")
+def redirectLogin():
+    return redirect('/login')
 
-@app.route("/logonFunction", methods=['POST'])
+@app.route("/login")
+def returnLogin(): 
+    return render_template('Log on.html')
+
+
+@app.route("/loginFunction", methods=['POST'])
 def logonFunction():
     if request.method == 'POST':
         username = request.form.get("username")
@@ -330,21 +332,46 @@ def logonFunction():
                 if isExist[0][0] == password:
                     global user
                     user = username
-                    return render_template("Main Page.html")
+                    print('Signed in as', user)
+                    return redirect("/home/" + user)
                 else:
                     message = "Username and password don't match"
                     print(message)
-                    return redirect('login')
+                    return redirect('/login')
             else:
                 message = "User not found"
                 print(message)
-                return redirect('login')
+                return redirect('/login')
         except Exception as e:
             cur.close()
             print(e)
             message = "Database error"
         print(message)
-        return redirect('login')
+        return redirect('/login')
+
+@app.route("/home/<user>")
+def returnHome(user):
+    try:
+        conn = sqlite3.connect("quizDatabase.db")
+        cur = conn.cursor()
+        cur.execute("SELECT FirstName, SurName FROM User WHERE Username = ?", (user,))
+        account = cur.fetchone()
+        cur.close()
+        print('Welcome,', account[0], account[1] )
+
+        if account:
+            firstName, surname = account[0], account[1]
+        else:
+            firstName, surname = user, ""
+            print('Error finding User')
+
+        return render_template("Main Page.html", user=user, firstName=firstName, surname=surname)
+    except Exception as e:
+        print(e)
+        print("Error accessing database")
+        return redirect('/login')
+
+
         
 if __name__ == "__main__":
     app.run(debug=True)
