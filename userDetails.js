9b946a65799ffb2bba5549119818663f0e0aa3bd
Benjamin Hockley <hockleyb@cardiff.ac.uk>
2023-11-27 10:59:18 +0000

    Delete userDetails.js

--- userDetails.js
+++
@@ -1,5 +0,0 @@
-// function to write new code in class boxes "quizName" and "quizScore"
-function printQuizName() {
-    document.getElementsByClassName("quizName")[0].innerHTML = "Quiz Name: Fun Quiz"
-    document.getElementsByClassName("quizScore")[0].innerHTML = "Quiz Score: 9/10"
-}
