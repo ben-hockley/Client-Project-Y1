e675146237351cdc6cb6c43bb56351fdb0e0dec0
Benjamin Hockley <hockleyb@cardiff.ac.uk>
2023-11-28 09:45:38 +0000

    delete duplicates from merge error

--- Server.py
+++ Server.py
@@ -450,6 +450,109 @@ def returnHome(user):
         print("Error accessing database")
         return redirect('/login')
 
+        QUIZLISTDATABASE = 'QuizList.db'
+
+@app.route("/listQuizzes")
+def printQuiz():
+    return render_template("ListQuizzes.html")
+
+@app.route("/QuizHistory/AddDefualtQuiz", methods = ['GET'])
+
+def quizAddDefaultDetails():
+
+	try:
+		conn = sqlite3.connect(QUIZLISTDATABASE)
+		cur = conn.cursor()
+
+		cur.execute("INSERT INTO QuizHistory ('quizName', 'genre', 'author')\
+					 VALUES (?,?,?)",("General Knowledge", "Social", "Ben") )
+		conn.commit()
+
+		msg = "Record successfully added"
+
+	except:
+
+		conn.rollback()
+
+		msg = "error in insert operation"
+
+	finally:
+		conn.close()
+
+		return msg
+
+
+def jls_extract_def():
+    return 'quizName'
+
+
+@app.route("/QuizHistory/AddQuiz", methods = ['POST','GET'])
+
+def quizAddDetails():
+
+	if request.method =='GET':
+
+		return render_template('QuizData.html')
+
+	if request.method =='POST':
+
+		quizName = request.form.get(jls_extract_def(), default="Error")#rem: args for get form for post
+		genre = request.form.get('genre', default="Error")
+		author = request.form.get('author', default="Error")
+		print("inserting quiz"+quizName)
+
+		try:
+			conn = sqlite3.connect(QUIZLISTDATABASE)
+			cur = conn.cursor()
+			cur.execute('INSERT INTO QuizHistory(quizName, genre, author) VALUES (?, ?, ?)', (quizName, genre, author))
+			conn.commit()
+
+			msg = "Record successfully added"
+
+		except:
+
+			conn.rollback()
+			msg = "error in insert operation"
+
+		finally:
+			conn.close()
+
+			return msg
+
+
+
+@app.route("/QuizHistory/Search", methods = ['GET','POST'])
+
+def quizSearch():
+
+	if request.method =='GET':
+
+		return render_template('QuizSearch.html')
+
+	if request.method =='POST':
+
+		try:
+
+			quizName = request.form.get('quizName', default="Error") #rem: args for get form for post
+
+			conn = sqlite3.connect(QUIZLISTDATABASE)
+			cur = conn.cursor()
+
+			# accepts both list[] or tuple() list mutable tuple not-mutable
+
+			cur.execute("SELECT * FROM QuizHistory WHERE quizName=? ;", [quizName])
+
+			data = cur.fetchall()
+			print(data)
+			print(data)
+		except:
+
+			print('there was an error', data)
+			conn.close()
+
+		finally:
+			conn.close()
+			return str(data)
 
 
 if __name__ == "__main__":
