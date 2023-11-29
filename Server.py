ea3b2e4d9c74122c214c25df2db66eca7138a157
alex stephens <stephensa10@cardiff.ac.uk>
2023-11-29 14:26:32 +0000

    Updated updateinfo and nav bar so that the mood emoji is displayed within the nav bar and updateinfo returns the mood emoji

--- Server.py
+++ Server.py
@@ -161,6 +161,24 @@ def getQuestion(QuizID):
             questions.append(question)
     return questions
 
+def getMoodEmoji(mood):
+    moodlist = ["&#128549;","&#128577;","&#128528;","&#128578;","&#128512;"]
+    return moodlist[mood]
+
+def getMood(user):
+    try:
+        conn = sqlite3.connect("quizDatabase.db")
+        cur = conn.cursor()
+        cur.execute("SELECT Mood FROM User WHERE Username = ?", (user,))
+        mood = cur.fetchone()[0]
+        mood = getMoodEmoji(mood)
+    except Exception as e:
+        conn.rollback()
+        print(e)
+        print("error retrieving mood")
+        mood = None
+    cur.close()
+    return mood
 
 @app.route("/moodChecker/<user>",methods=['GET','POST'])
 def updateMood(user):
@@ -230,9 +248,11 @@ def updateInfo(user):
         conn = sqlite3.connect('quizDatabase.db')
         cur = conn.cursor()
         cur.execute("\
-        SELECT FirstName, SurName, Username FROM User WHERE Username = ?",([user]))
-        details = cur.fetchall()
-        newList = json.dumps(details[0])
+        SELECT FirstName, SurName, Username, Mood FROM User WHERE Username = ?",([user]))
+        details = cur.fetchall()[0]
+        firstname, surname, username, mood = details[0], details[1], details[2], details[3]
+        details = [firstname, surname, username, getMoodEmoji(mood)]
+        newList = json.dumps(details)
         conn.close()
         return newList
     except Exception as e:
@@ -462,14 +482,14 @@ def returnHome(user):
         account = cur.fetchone()
         cur.close()
         print('Welcome,', account[0], account[1] )
-
+        mood = getMood(user)
         if account:
             firstName, surname = account[0], account[1]
         else:
             firstName, surname = user, ""
             print('Error finding User')
 
-        return render_template("Main Page.html", user=user, firstName=firstName, surname=surname)
+        return render_template("Main Page.html", user=user, firstName=firstName, surname=surname, mood=mood)
     except Exception as e:
         print(e)
         print("Error accessing database")
