45bd347807a7a3a5e7a5c2545075b5132a34cfc2
Benjamin Hockley <hockleyb@cardiff.ac.uk>
2023-11-29 18:12:53 +0000

    fixed button so quiz history prints

--- Server.py
+++ Server.py
@@ -481,9 +481,8 @@ def quizSearch(user):
             print(userID)
             cur.execute("SELECT QuizName FROM Quiz WHERE UserID = ?", (userID,))
             QuizHistory = cur.fetchall()
-            quiz_history = json.dumps(QuizHistory)
             QuizzesPlayed = str(len(QuizHistory))
-            return render_template("Quiz History.html", user=user, QuizzesPlayed=QuizzesPlayed, QuizHistory=QuizHistory),quiz_history
+            return render_template("Quiz History.html", user=user, QuizzesPlayed=QuizzesPlayed, QuizHistory=QuizHistory)
         except:
             print('there was an error')
         conn.close()
