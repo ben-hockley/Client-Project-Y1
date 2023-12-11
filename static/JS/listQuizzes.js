3bdb11d340b356212bff52880383be17a27aa855
alex stephens <stephensa10@cardiff.ac.uk>
2023-12-11 11:48:04 +0000

    used kebab-case for css

--- static/JS/listQuizzes.js
+++ static/JS/listQuizzes.js
@@ -5,7 +5,7 @@ function updateQuizMoods() {
     $.ajax({type:'GET', url:newRoute, success: function appendInfo(result) {
         result = JSON.parse(result);
         for (let i=0; i < Object.keys(result).length; i++) {
-        row = "<section class='newBox' id='" + i + "'><section class='quizName'><p>" + result[i][0] + "</p></section><section class='quizCode'><p>" + result[i][1] + "</section>"
+        row = "<section class='new-box' id='" + i + "'><section class='quiz-name'><p>" + result[i][0] + "</p></section><section class='quiz-code'><p>" + result[i][1] + "</section>"
         $("#quizzes").append(row);
         document.getElementById(i).addEventListener("click", function() {
             user = window.location.pathname.split("/").pop();
