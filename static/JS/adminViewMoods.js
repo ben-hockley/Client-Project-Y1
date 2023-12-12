0869fa3faa285429de4793786a87968255e7b203
alex stephens <stephensa10@cardiff.ac.uk>
2023-12-12 14:45:31 +0000

    Vertically centered text on the admin mood page

--- static/JS/adminViewMoods.js
+++ static/JS/adminViewMoods.js
@@ -9,7 +9,7 @@ function updateQuizMoods() {
         $("#adminQuizMoods").html("");
         $("#adminQuizMoods").append("<tr><td>Quiz Title</td><td>Username</td><td>Mood Before</td><td>Mood After</td></tr>")
         for (let i=0; i < Object.keys(result).length; i++) {
-            row = "<tr><td class='form-text'>" + result[i][0] + "</td><td class='form-text'>" + result[i][1] + "</td><td class='emoji'>" + emojiList[result[i][2]] + "</td><td class='emoji'>" + emojiList[result[i][3]] + "</td></tr>";
+            row = "<tr><td class='form-text'><section class='vertical-center'>" + result[i][0] + "</section></td><td class='form-text'><section class='vertical-center'>" + result[i][1] + "</section></td><td class='emoji'><section class='vertical-center'>" + emojiList[result[i][2]] + "</section></td><td class='emoji'><section class='vertical-center'>" + emojiList[result[i][3]] + "</section></td></tr>";
             $("#adminQuizMoods").append(row);
     }
     }});
