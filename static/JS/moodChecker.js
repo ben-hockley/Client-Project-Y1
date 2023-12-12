9b99d2ab8ed3534d2a73a35aae0e621f4814e700
alex stephens <stephensa10@cardiff.ac.uk>
2023-12-12 15:43:47 +0000

    Added in 4 new emojis and implemented them across the site

--- static/JS/moodChecker.js
+++ static/JS/moodChecker.js
@@ -1,6 +1,6 @@
 var slider = document.getElementById("moodSlider");
 var emoji = document.getElementById("emoji");
-let emojiList = ["&#128549","&#128577","&#128528","&#128578","&#128512"];
+let emojiList = ["&#128545","&#128544","&#128546","&#128577","&#128528","&#128578","&#128512","&#129321","&#129322"];
 slider.oninput=function() {
     emoji.innerHTML = emojiList[slider.value];
 }
--- static/JS/viewMoods.js
+++ static/JS/viewMoods.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", updateQuizMoods())
 
 function updateQuizMoods() {
-    let emojiList = ["&#128549","&#128577","&#128528","&#128578","&#128512"];
+    let emojiList = ["&#128545","&#128544","&#128546","&#128577","&#128528","&#128578","&#128512","&#129321","&#129322"];
     user = window.location.pathname.split("/").pop();
     newRoute = "/updateQuizMood/" + user;
     $.ajax({type:'GET', url:newRoute, success: function appendInfo(result) {
